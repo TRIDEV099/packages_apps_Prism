Lunaris-AOSP/packages_apps_Singularity

Switch to Axion color picker implementation
- rmp22 for base implementation https://github.com/AxionAOSP/android_packages_apps_AxThemePicker
- Ghost adapt for settings from AxThemePicker
- Skydoves for olor picker library https://github.com/skydoves/colorpicker-compose

Change-Id: I460b6b78b83798924acee5c4964c971f0cdf8ac1
Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
Co-authored-by: Jaewoong Eum <skydoves@naver.com>
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

src/org/lunaris/settings/fragments/themes/ColorPickerDialogFragment.kt
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2024-2025 Lunaris AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lunaris.settings.fragments.themes
+
+import android.graphics.Color as AndroidColor
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.DialogFragment
+import org.lunaris.settings.utils.ColorPickerDialog
+import org.lunaris.settings.utils.LunarisTheme
+
+class ColorPickerDialogFragment : DialogFragment() {
+
+    private var initialColor: Color = Color(0xFF6750A4)
+    private var onColorSelected: ((Color) -> Unit)? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, android.R.style.Theme_Material_Dialog_NoActionBar)
+        
+        arguments?.getString(ARG_COLOR_HEX)?.let { hex ->
+            try {
+                initialColor = Color(AndroidColor.parseColor("#$hex"))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            
+            setContent {
+                LunarisTheme {
+                    ColorPickerDialog(
+                        initialColor = initialColor,
+                        onDismiss = { dismiss() },
+                        onColorSelected = { color ->
+                            onColorSelected?.invoke(color)
+                            dismiss()
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+    fun setOnColorSelectedListener(listener: (Color) -> Unit) {
+        onColorSelected = listener
+    }
+
+    companion object {
+        private const val ARG_COLOR_HEX = "color_hex"
+        const val TAG = "ColorPickerDialogFragment"
+
+        fun newInstance(colorHex: String): ColorPickerDialogFragment {
+            return ColorPickerDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_COLOR_HEX, colorHex)
+                }
+            }
+        }
+    }
+}

src/org/lunaris/settings/fragments/themes/WallpaperColorPickerDialogFragment.kt
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2024-2025 Lunaris AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lunaris.settings.fragments.themes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.DialogFragment
+import org.lunaris.settings.utils.WallpaperColorPickerDialog
+import org.lunaris.settings.utils.LunarisTheme
+
+class WallpaperColorPickerDialogFragment : DialogFragment() {
+
+    private var onColorSelected: ((Color) -> Unit)? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, android.R.style.Theme_Material_Dialog_NoActionBar)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            
+            setContent {
+                LunarisTheme {
+                    WallpaperColorPickerDialog(
+                        onDismiss = { dismiss() },
+                        onColorSelected = { color ->
+                            onColorSelected?.invoke(color)
+                            dismiss()
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+    fun setOnColorSelectedListener(listener: (Color) -> Unit) {
+        onColorSelected = listener
+    }
+
+    companion object {
+        const val TAG = "WallpaperColorPickerDialogFragment"
+
+        fun newInstance(): WallpaperColorPickerDialogFragment {
+            return WallpaperColorPickerDialogFragment()
+        }
+    }
+}

src/org/lunaris/settings/utils/ColorUtils.kt
@@ -1,0 +1,30 @@
+package org.lunaris.settings.utils
+
+import android.graphics.Color as GraphicsColor
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+
+fun Color.toArgb(): Int {
+    return GraphicsColor.argb(
+        (alpha * 255).toInt(),
+        (red * 255).toInt(),
+        (green * 255).toInt(),
+        (blue * 255).toInt()
+    )
+}
+
+fun Color.getContentColor(): Color {
+    return if (luminance() > 0.5f) Color.Black else Color.White
+}
+
+fun String.parseColorHex(): Color? {
+    return try {
+        Color(GraphicsColor.parseColor(if (startsWith("#")) this else "#$this"))
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun Color.toHexString(): String {
+    return String.format("%06X", 0xFFFFFF and toArgb())
+}

src/org/lunaris/settings/utils/ThemeStyle.kt
@@ -1,0 +1,11 @@
+package org.lunaris.settings.utils
+
+enum class ThemeStyle(val displayName: String, val systemValue: String) {
+    TONAL_SPOT("Tonal Spot", "TONAL_SPOT"),
+    VIBRANT("Vibrant", "VIBRANT"),
+    EXPRESSIVE("Expressive", "EXPRESSIVE"),
+    SPRITZ("Spritz", "SPRITZ"),
+    RAINBOW("Rainbow", "RAINBOW"),
+    FRUIT_SALAD("Fruit Salad", "FRUIT_SALAD"),
+    MONOCHROMATIC("Monochromatic", "MONOCHROMATIC")
+}

TRIDEV099/packages_apps_Prism

Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

src/com/prism/settings/fragments/themes/ColorPickerDialogFragment.kt
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2024-2025 Lunaris AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.prism.settings.fragments.themes
+
+import android.graphics.Color as AndroidColor
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.DialogFragment
+import com.prism.settings.utils.ColorPickerDialog
+import com.prism.settings.utils.EuclidTheme
+
+class ColorPickerDialogFragment : DialogFragment() {
+
+    private var initialColor: Color = Color(0xFF6750A4)
+    private var onColorSelected: ((Color) -> Unit)? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, android.R.style.Theme_Material_Dialog_NoActionBar)
+        
+        arguments?.getString(ARG_COLOR_HEX)?.let { hex ->
+            try {
+                initialColor = Color(AndroidColor.parseColor("#$hex"))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            
+            setContent {
+                EuclidTheme {
+                    ColorPickerDialog(
+                        initialColor = initialColor,
+                        onDismiss = { dismiss() },
+                        onColorSelected = { color ->
+                            onColorSelected?.invoke(color)
+                            dismiss()
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+    fun setOnColorSelectedListener(listener: (Color) -> Unit) {
+        onColorSelected = listener
+    }
+
+    companion object {
+        private const val ARG_COLOR_HEX = "color_hex"
+        const val TAG = "ColorPickerDialogFragment"
+
+        fun newInstance(colorHex: String): ColorPickerDialogFragment {
+            return ColorPickerDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_COLOR_HEX, colorHex)
+                }
+            }
+        }
+    }
+}

src/com/prism/settings/fragments/themes/WallpaperColorPickerDialogFragment.kt
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2024-2025 Lunaris AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.prism.settings.fragments.themes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.DialogFragment
+import com.prism.settings.utils.WallpaperColorPickerDialog
+import com.prism.settings.utils.EuclidTheme
+
+class WallpaperColorPickerDialogFragment : DialogFragment() {
+
+    private var onColorSelected: ((Color) -> Unit)? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, android.R.style.Theme_Material_Dialog_NoActionBar)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            
+            setContent {
+                EuclidTheme {
+                    WallpaperColorPickerDialog(
+                        onDismiss = { dismiss() },
+                        onColorSelected = { color ->
+                            onColorSelected?.invoke(color)
+                            dismiss()
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+    fun setOnColorSelectedListener(listener: (Color) -> Unit) {
+        onColorSelected = listener
+    }
+
+    companion object {
+        const val TAG = "WallpaperColorPickerDialogFragment"
+
+        fun newInstance(): WallpaperColorPickerDialogFragment {
+            return WallpaperColorPickerDialogFragment()
+        }
+    }
+}

src/com/prism/settings/utils/ColorUtils.kt
@@ -1,0 +1,30 @@
+package com.prism.settings.utils
+
+import android.graphics.Color as GraphicsColor
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+
+fun Color.toArgb(): Int {
+    return GraphicsColor.argb(
+        (alpha * 255).toInt(),
+        (red * 255).toInt(),
+        (green * 255).toInt(),
+        (blue * 255).toInt()
+    )
+}
+
+fun Color.getContentColor(): Color {
+    return if (luminance() > 0.5f) Color.Black else Color.White
+}
+
+fun String.parseColorHex(): Color? {
+    return try {
+        Color(GraphicsColor.parseColor(if (startsWith("#")) this else "#$this"))
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun Color.toHexString(): String {
+    return String.format("%06X", 0xFFFFFF and toArgb())
+}

src/com/prism/settings/utils/ThemeStyle.kt
@@ -1,0 +1,11 @@
+package com.prism.settings.utils
+
+enum class ThemeStyle(val displayName: String, val systemValue: String) {
+    TONAL_SPOT("Tonal Spot", "TONAL_SPOT"),
+    VIBRANT("Vibrant", "VIBRANT"),
+    EXPRESSIVE("Expressive", "EXPRESSIVE"),
+    SPRITZ("Spritz", "SPRITZ"),
+    RAINBOW("Rainbow", "RAINBOW"),
+    FRUIT_SALAD("Fruit Salad", "FRUIT_SALAD"),
+    MONOCHROMATIC("Monochromatic", "MONOCHROMATIC")
+}